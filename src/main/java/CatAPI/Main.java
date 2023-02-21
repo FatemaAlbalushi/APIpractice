f116b7f0f5cc9fed7b30044c3029af408ac72536
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-02-21 11:02:01 +0400

    Cat API

--- src/main/java/CatAPI/Main.java
+++ src/main/java/CatAPI/Main.java
@@ -25,23 +25,6 @@ public class Main {
             String uniqueFilename = fileManager.generateUniqueFilename();
             CatImage catImage = new CatImage(catImageUrl, uniqueFilename);
             catImage.saveToFile(fileManager);
-
-            // Get a list of all saved cat images
-            List<String> savedImageFilenames = new ArrayList<>();
-            Path dataDirectory = Path.of(fileManager.getDataDirectoryPath());
-            Files.walk(dataDirectory)
-                    .filter(Files::isRegularFile)
-                    .forEach(file -> savedImageFilenames.add(file.toString()));
-
-            // Choose a random image from the list
-            if (savedImageFilenames.size() > 0) {
-                String randomImageFilename = savedImageFilenames.get(new Random().nextInt(savedImageFilenames.size()));
-                System.out.println("Displaying image: " + randomImageFilename);
-                // code to display the image
-            } else {
-                System.out.println("No saved cat images found.");
-            }
-
         } catch (IOException e) {
             System.err.println("An error occurred while fetching or saving the cat image: " + e.getMessage());
         }
